f3a227f32a53ea56432aa98a255654f54eb2f210
Patrick Badiang <patrickvyn@gmail.com>
2024-06-29 23:17:08 -0400

    Made appointments lists and added dog png

--- lib/appointmentpage.dart
+++ lib/appointmentpage.dart
@@ -1,21 +1,52 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pet_care_app/models/appointmentModel.dart';
 import 'package:pet_care_app/topWidget.dart';
 
 class Appointments extends StatelessWidget {
-  const Appointments({super.key});
+  Appointments({super.key});
+
+  List appointments = [
+    ["assets/images/needle.png", "Rabies Shot TBD", Color(0xffCA7676)],
+    ["assets/images/dog.png", "Nail Clippings", Color(0xffCAA376)],
+    ["assets/images/needle.png", "Rabies Shot TBD", Color(0xffCA7676)],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.small(
+        shape: CircleBorder(),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          print("Clicked");
+        },
+        child: Icon(Icons.add),
+      ),
+      body: Column(
         children: <Widget>[
-          TopWidget(subText: "", title: "My Appointments:"),
-          SizedBox(height: 20,),
-          AppointmentModel(name: "Rabies Shot TBD"),
+          const TopWidget(subText: "", title: "My Appointments:"),
+          const SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              separatorBuilder: (context, index) => SizedBox(height: 10),
+              itemCount: appointments.length,
+              itemBuilder: (context, index) {
+                return AppointmentModel(
+                  path: appointments[index][0],
+                  name: appointments[index][1],
+                  bgcolor: appointments[index][2],
+                );
+              },
+            ),
+          ),
         ],
-      
+      ),
     );
   }
 }
--- lib/models/appointmentModel.dart
+++ lib/models/appointmentModel.dart
@@ -1,29 +1,36 @@
 import 'package:flutter/material.dart';
 
 class AppointmentModel extends StatelessWidget {
+  final String path;
   final String name;
+  final Color  bgcolor;
 
-  const AppointmentModel({super.key, required this.name});
+  const AppointmentModel({
+    super.key,
+    required this.name,
+    required this.path,
+    required this.bgcolor,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 80.0,
-      width: 350.0,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        color: Color(0xffF7FEF2),
+        color: Color(0xffD9D9D9),
       ),
       child: Row(
         children: [
           Container(
+            alignment: Alignment.center,
             height: 100,
             width: 100,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              color: Color(0xffCA7676),
+              color: bgcolor,
             ),
-            child: Image.asset("assets/images/needle.png"),
+            child: Image.asset(path),
           ),
           SizedBox(width: 10),
           Text(name),
